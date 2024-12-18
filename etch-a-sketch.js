69ac4573ca913d7be1982aaf0ac9cc123560da8a
Nick <nikitachabanets@gmail.com>
2024-12-18 23:57:01 +0100

    Create 16x16 grid of square divs for webpage layout

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const container = document.querySelector(".container");
+
+function makeDiv(){
+    for(let i = 0; i < 17; i++){
+        const gridWrapper = document.createElement("div");
+        for(let i = 0; i < 17; i++){
+            const gridItem = document.createElement("div");
+            gridItem.setAttribute("class", "add");
+            gridWrapper.appendChild(gridItem);
+            container.appendChild(gridWrapper);
+        }
+    }
+}
+
+makeDiv();
