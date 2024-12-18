70571dda66fe788610c7577823f52d1ec554d0a9
Nick <nikitachabanets@gmail.com>
2024-12-19 00:04:15 +0100

    Add hover effect to clolor grid items and a popup to select number of squares per side

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,16 @@
 const container = document.querySelector(".container");
+let numberOfSquares = prompt("Enter the number of squares per side for the new grid", "");
 
 function makeDiv(){
-    for(let i = 0; i < 17; i++){
+    for(let i = 0; i < numberOfSquares; i++){
         const gridWrapper = document.createElement("div");
-        for(let i = 0; i < 17; i++){
+        for(let i = 0; i < numberOfSquares; i++){
             const gridItem = document.createElement("div");
             gridItem.setAttribute("class", "add");
             gridWrapper.appendChild(gridItem);
             container.appendChild(gridWrapper);
+            gridItem.addEventListener("mouseenter", () => { gridItem.setAttribute("style", "background-color: yellow;") });
+
         }
     }
 }
