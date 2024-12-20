9203dd582a1ac2c031abd1978c37f99fede7ca61
Nick <nikitachabanets@gmail.com>
2024-12-20 20:37:54 +0100

    Add button to generate customizable grid with popup input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,37 @@
 const container = document.querySelector(".container");
-let numberOfSquares = prompt("Enter the number of squares per side for the new grid", "");
+const btn = document.querySelector("button");
 
-function makeDiv(){
+let numberOfSquares = 10;
+
+function setNumberOfSquares(){
+    numberOfSquares = prompt("Enter the number of squares per side for the new grid", "");
+
+    if(numberOfSquares < 1 || numberOfSquares === null){
+        numberOfSquares = 16;
+    }
+}
+
+btn.addEventListener("click", () => {
+    setNumberOfSquares();
+    let allGridWrappers = document.querySelectorAll(".gridWrapper");
+    allGridWrappers.forEach(element => {
+        container.removeChild(element);
+    });
+    makeDiv(numberOfSquares);
+});
+
+function makeDiv(numberOfSquares){
     for(let i = 0; i < numberOfSquares; i++){
         const gridWrapper = document.createElement("div");
+        gridWrapper.setAttribute("class", "gridWrapper");
         for(let i = 0; i < numberOfSquares; i++){
             const gridItem = document.createElement("div");
-            gridItem.setAttribute("class", "add");
+            gridItem.setAttribute("class", "gridItem");
             gridWrapper.appendChild(gridItem);
             container.appendChild(gridWrapper);
-            gridItem.addEventListener("mouseenter", () => { gridItem.setAttribute("style", "background-color: #B28474;") });
-
+            gridItem.addEventListener("mouseenter", () => { gridItem.setAttribute("style", "background-color: #B28474;"); });
         }
     }
 }
 
-makeDiv();
+makeDiv(numberOfSquares);
